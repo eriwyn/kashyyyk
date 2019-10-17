ad5e3ce23cb824c5e061a0af4f5f2d4b74107030
xavier albic <xavier.albic2@etu.univ-lorraine.fr>
2019-10-17 17:50:24 +0200

    component header

---
+++ src/js/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Header extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return <header><h1>Kashyyyk</h1></header>}
+
+}
+export default Header;
