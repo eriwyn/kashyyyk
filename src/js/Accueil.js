652d3c144023d706270b3eb53b4c52d34df88100
xavier albic <xavier.albic2@etu.univ-lorraine.fr>
2019-12-04 17:53:46 +0100

    modifs css tout beau tout propre pour l'accueil

--- src/js/Accueil.js
+++ src/js/Accueil.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/Accueil.scss';
+import '../css/button.scss';
 class Accueil extends React.Component{
   constructor(props){
     super(props);
@@ -7,22 +8,20 @@ class Accueil extends React.Component{
   render(){
 
 
-    return  <main className="MainAccueil">
+    return  <main className="mainPage">
       <h1>Kashyyyk</h1>
-
-        <p className="paragraphePresentationAccueil">Créez des formulaires, des tableaux et des menus personnalisés</p>
+        <p className="paragraphePresentationAccueil">Créez vos formulaires , tableaux et menus</p>
         <p className="paragraphePresentationAccueil">Exportez vos propres données</p>
         <p className="paragraphePresentationAccueil">Une prise en main rapide et gratuite</p>
-    
       <div className="divButton">
-          <a href="#" className="buttonAccueil" id="buttonForm"><h2>Créer un formulaire</h2></a>
-          <a href="#" className="buttonAccueil" id="buttonTableau"><h2>Créer un tableau</h2></a>
-          <a href="#" className="buttonAccueil" id="buttonMenu"><h2>Créer un menu</h2></a>
+          <a href="#" className="ghostButton" id="buttonForm">Créer un formulaire</a>
+          <a href="#" className="ghostButton" id="buttonTableau">Créer un tableau</a>
+          <a href="#" className="ghostButton" id="buttonMenu">Créer un menu</a>
       </div>
-      <div className="divFlexCenter">
+      <div className="divCompteAccueil">
         <h2 className="h2AccueilCompte">Vous voulez sauvegarder votre travail ?</h2>
         <p>Créez un compte pour découvrir cette fonctionnalité</p>
-        <a href="#" className="buttonAccueil"><h2>Créer un compte</h2></a>
+        <a href="#" className="ghostButton">Créer un compte</a>
       </div>
     </main>
 
