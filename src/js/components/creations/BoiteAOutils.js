1fdb266624e45221175c46fcdaa973d53d992301
eriwyn <valentin@medialsace.fr>
2020-01-13 14:31:33 +0100

    Début du formulaire

--- src/js/components/creations/BoiteAOutils.js
+++ src/js/components/creations/BoiteAOutils.js
@@ -1,10 +1,10 @@
 import React  from 'react';
-import '../css/Creation.scss';
+import '../../../css/Creation.scss';
 
 
 export default function BoiteAOutils() {
 
-    return <div>
+    return <div className="boiteAOutils">
         
     </div>
 }
