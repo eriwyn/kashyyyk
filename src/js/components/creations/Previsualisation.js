84fde12aa42321c2e950f42d43691d4b8a0ff5c3
Eriwyn <valentin@medialsace.fr>
2020-03-01 15:03:21 +0100

    Suppression de uniqid

--- src/js/components/creations/Previsualisation.js
+++ src/js/components/creations/Previsualisation.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect}  from 'react';
 import '../../../css/Creation.scss';
 import Element from './Element.js';
 import { connect } from "react-redux";
-import uniqid from "uniqid";
 
 
 const Previsualisation = props => {
