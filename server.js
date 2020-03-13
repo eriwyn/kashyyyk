7e09dfb84b5bf7f8a21e3addb84920527f0c6d83
Eriwyn <valentin@medialsace.fr>
2020-03-13 15:10:22 +0100

    trucs

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
